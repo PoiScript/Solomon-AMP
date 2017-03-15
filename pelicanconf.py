182e4c3b7112dc13f3f457e8488ec79efc579780
PoiScript <poiscript@gmail.com>
2017-03-15 23:18:09 +0800

    chore: initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'PoiScript'
+SITENAME = 'Solomon'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Beijing'
+
+DEFAULT_LANG = 'zh'
+
+THEME = 'theme'
+
+STATIC_PATHS = ['extra/favicon.ico', 'extra/icon.png']
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
